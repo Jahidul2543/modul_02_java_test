7a95982d5892fafd71da687f0693aeca71a41441
kanon-nyc <57508437+kanon-nyc@users.noreply.github.com>
2020-01-04 14:38:24 -0500

    Update DeepDiveArrayList.java

--- module_02_test/src/main/java/api/DeepDiveArrayList.java
+++ module_02_test/src/main/java/api/DeepDiveArrayList.java
@@ -11,6 +11,29 @@ public class DeepDiveArrayList {
      * You are free to document your understanding using comment
      *
      * */
+    public static void main(String[]args) {
+		
+		int a[]=  new int[3];//static array
+		//dynamic array--arraylist
+		
+	ArrayList ar= new ArrayList();
+		ar.add(10);
+		ar.add(20);
+		ar.add(30);
+		System.out.println(ar.size());
+		ar.add(40);
+		ar.add(50);
+		ar.add(12.33);
+		ar.add("Test");
+		ar.add('a');
+		ar.add(true);
+		ar.add(false);
+		System.out.println(ar.size());
+		System.out.println(ar.get(4));
+		
+		for(int i=0;i<ar.size();i++){
+			System.out.println(ar.get(i));
+		}
 
 
 }
